58a999e82c4185b4abecc135eec65212688b0133
Sumanta Gogoi <sumanta@xynocast.com>
2023-01-18 23:09:06 +0530

    no message

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.apps import apps
+
+
+myapp = apps.get_app_config('ticketing')
+admin.site.register(myapp.get_models())
