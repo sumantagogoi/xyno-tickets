75f684f6a1f9f6bccb8ca4d587de941a6c1d2808
Sumanta Gogoi <sumanta@xynocast.com>
2023-01-18 23:00:29 +0530

    no message

--- ticketing/models.py
+++ ticketing/models.py
@@ -10,28 +10,6 @@ class Client(models.Model):
     address = models.CharField(max_length=300, blank=True,null=True)
     notes = models.CharField(max_length=300, blank=True,null=True)
 
-
-class Event(models.Model):
-    client = models.ForeignKey(Client)  
-    name = models.CharField(max_length=100)
-    event_creative = models.ImageField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField(blank=True,null=True)      
-    status = models.BooleanField(default = True)
-
-
-class Ticket (models.Model):
-    event = models.ForeignKey(Event)   
-    ticket_cat =  models.CharField(max_length=100)
-    ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
-    max_tickets = models.IntegerField(blank=True,null=True)
-
-
-class TSaleCounter(models.Model):
-    ticket = models.ForeignKey(Ticket,unique=True)
-    number_sold = models.IntegerField()
-
-
 class Customer(models.Models):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
@@ -40,10 +18,31 @@ class Customer(models.Models):
     age = models.CharField(max_length=100, blank=True,null=True)
 
 
+class Event(models.Model):
+    client = models.ForeignKey(Client, on_delete=DO_NOTHING)  
+    name = models.CharField(max_length=100)
+    event_creative = models.ImageField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True,null=True)      
+    status = models.BooleanField(default = True)
+
+
+class Ticket (models.Model):
+    event = models.ForeignKey(Event, on_delete=DO_NOTHING)  
+    ticket_cat =  models.CharField(max_length=100)
+    ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
+    max_tickets = models.IntegerField(blank=True,null=True)
+
+
+class TSaleCounter(models.Model):
+    ticket = models.ForeignKey(Ticket,unique=True, on_delete=DO_NOTHING)  
+    number_sold = models.IntegerField()
+
+
 class TSoldData(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    ticket = models.ForeignKey(Ticket)
-    customer =  models.ForeignKey(Customer)
+    ticket = models.ForeignKey(Ticket, on_delete=DO_NOTHING)  
+    customer =  models.ForeignKey(Customer, on_delete=DO_NOTHING)  
     quantity = models.IntegerField(default = 1)
     txn_details = models.JSONField()
     status = models.CharField(choices=(("claimed", "claimed"),("booked", "booked"),("cancelled", "cancelled"),("refunded", "refunded")))
