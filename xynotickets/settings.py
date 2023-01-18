3a4e79267cfdffdecdbb6b98b744ac001fb7db7e
Sumanta Gogoi <sumanta@xynocast.com>
2023-01-18 21:47:55 +0530

    no message

--- xynotickets/settings.py
+++ xynotickets/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = '/var/www/xynotickets/static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
